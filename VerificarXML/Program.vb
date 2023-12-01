1c8e9c11a74410f302f7b5e10065bebe6fe8ffab
GabrielRibeirovoya <gabrielsribeiro3312@gmail.com>
2023-12-01 09:28:22 -0300

    Adicionando todos os valores

--- VerificarXML/Program.vb
+++ VerificarXML/Program.vb
@@ -1,6 +1,7 @@
 Imports System
 Imports System.ComponentModel.Design
 Imports System.Drawing
+Imports System.Globalization
 Imports System.IO
 Imports System.Xml
 Imports SNMPNET
@@ -9,11 +10,35 @@ Module Program
     Sub Main(args As String())
         Do
 
-
-            Dim verificar As New Impressoras
-            Console.Write("Deseja puxar todos os valores de um XML ou somente um especifico? (1 para todos e 2 para valor especifico)")
+            Console.WriteLine("Deseja puxar todos os valores de um XML ou somente um especifico (1 para todos e 2 para valor especifico)? ")
             If Console.ReadLine = 1 Then
+                Dim xmlDoc As New XmlDocument()
+                Console.Write("Digite o caminho do arquivo: ")
+                Dim caminho As String = Console.ReadLine
+                Dim opção As String
+                Dim endereco As String
+                If caminho = Nothing Then
+                    Console.WriteLine("Arquivo não encontrado")
+                Else
+                    Do
+                        Try
+                            xmlDoc.Load(caminho)
+                            Console.WriteLine("Digite o nome do elemento: ")
+                            endereco = xmlDoc.SelectSingleNode(Console.ReadLine).InnerText
+                            Console.WriteLine(endereco + " ")
 
+                            Console.WriteLine("Deseja procurar mais algum arquivo XML? (S\N): ")
+                            opção = Console.ReadLine
+                            If opção = "N" Or opção = "n" Then
+                                Exit Do
+                            End If
+                        Catch ex As Exception
+                            Console.WriteLine("Elemento não encontrado!")
+                            Exit Do
+                        End Try
+                    Loop
+
+                End If
             ElseIf Console.ReadLine = 2 Then
                 Console.Write("Digite o caminho do arquivo: ")
                 Dim caminho As String = Console.ReadLine
